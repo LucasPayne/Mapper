6d19505e0fee7062951c027ae85df6bfe08653e3
lucaspayne <lucaspayyne@gmail.com>
2021-06-09 17:47:35 +1200

    coordinate inversion bug ...

--- src/MapperViewer_main.cpp
+++ src/MapperViewer_main.cpp
@@ -5,59 +5,65 @@
 #include <string>
 #include <math.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/quaternion.hpp>
 #include "glm/glm.hpp"
 #include "lib_cg_sandbox/core/cg_sandbox.h"
 #include "mesh_processing/mesh_processing.h"
+#include <Eigen/Dense>
+
+// toggleable options
+bool view_depth = false;
+
+struct CameraIntrinsics {
+    double fx;
+    double fy;
+    double cx;
+    double cy;
+    int pixels_x;
+    int pixels_y;
+    double depth_scale;
+    mat3x3 intrinsic_matrix;
+    mat3x3 intrinsic_matrix_inverse;
+} intrinsics;
 
 
 // Globals
 Aspect<Camera> main_camera;
 struct FrameMetadata {
     // 1341847980.790000 -0.6832 2.6909 1.7373 0.0003 0.8617 -0.5072 -0.0145 1341847980.786879 depth/1341847980.786879.png 1341847980.786856 rgb/1341847980.786856.png
-    vec3 position;
-    vec3 rotation;
+    vec3 world_position;
     double t;
     std::string depth_file;
     std::string rgb_file;
+    GLuint rgb_tex;
+    GLuint depth_tex;
+    Image<vec4> rgb_image;
+    Image<vec4> depth_image;
+
+    mat3x3 rotation; // extrinsic_matrix is [R|t]
+    mat4x4 extrinsic_matrix;
+
+    mat3x3 intrinsic_matrix;
+    mat3x3 intrinsic_matrix_inverse;
 };
 std::vector<FrameMetadata> frame_metadata;
 
 
-// Mesh loading.
-Entity create_mesh_object(World &world,
-                          MLModel &model,
-                          const std::string &mat_path)
+vec3 intrinsic_point(double u, double v, double z)
 {
-    if (!model.has_normals) model.compute_phong_normals();
-
-    Entity e = world.entities.add();
-    auto t = e.add<Transform>(0,0,0);
-    auto box = BoundingBox(model.positions);
-    auto sphere = box.bounding_sphere();
-    
-    Resource<GeometricMaterial> gmat = world.graphics.shading.geometric_materials.load("shaders/triangle_mesh/triangle_mesh.gmat");
-    Resource<Material> mat = world.graphics.shading.materials.load(mat_path);
-
-    VertexArrayData vad;
-    MLModel_to_VertexArrayData(model, vad);
-
-    auto va = world.resources.add<VertexArray>();
-    *va = VertexArray::from_vertex_array_data(vad);
-
-    // Resource<VertexArray> model_vertex_array = world.assets.models.load(model_path);
-    auto gmat_instance = GeometricMaterialInstance(gmat, va);
-    auto mat_instance = MaterialInstance(mat);
-    auto drawable = e.add<Drawable>(gmat_instance, mat_instance);
-    drawable->raw_bounding_sphere = sphere;
-    return e;
+    return vec3(((intrinsics.pixels_x - 1) * u - intrinsics.cx) * z/intrinsics.fx,
+	        ((intrinsics.pixels_y - 1) * v - intrinsics.cy) * z/intrinsics.fy, z);
 }
-Entity create_mesh_object(World &world,
-                          const std::string &model_path,
-                          const std::string &mat_path)
+vec3 frame_to_world(const FrameMetadata &md, vec3 uvz)
 {
-    auto model = MLModel::load(model_path);
-    return create_mesh_object(world, model, mat_path);
+    // vec3 cameraspace_point = intrinsics.intrinsic_matrix_inverse * uvz;
+    // return (md.extrinsic_matrix_inverse * vec4(cameraspace_point, 1)).xyz();
+
+    //---I think fastfusion uses an incorrect notion of the extrinsic matrix...
+    return md.world_position + md.rotation * intrinsic_point(uvz.x(), uvz.y(), uvz.z());
 }
+
+
 // Camera
 struct CameraController : public IBehaviour {
     float azimuth;
@@ -174,44 +180,146 @@ Entity create_cameraman(World &world)
 }
 
 
+Image<vec4> cv_matrix_to_image_rgba(cv::Mat &cv_img)
+{
+    auto img = Image<vec4>(cv_img.rows, cv_img.cols);
+    for (int i = 0; i < cv_img.rows; i++) {
+        for (int j = 0; j < cv_img.cols; j++) {
+            auto rgb = cv_img.at<cv::Vec3b>(i, j);
+            img(i,j) = vec4(rgb[0]/256.f, rgb[1]/256.f, rgb[2]/256.f, 1);
+        }
+    }
+    return img;
+}
+Image<vec4> depth_to_distance_image(cv::Mat &cv_img)
+{
+    auto img = Image<vec4>(cv_img.rows, cv_img.cols);
+    for (int i = 0; i < cv_img.rows; i++) {
+        for (int j = 0; j < cv_img.cols; j++) {
+            auto color = cv_img.at<unsigned short>(i, j);
+            float scaled = color / intrinsics.depth_scale;
+            img(i,j) = vec4(scaled, scaled, scaled, 1);
+        }
+    }
+    return img;
+}
+Image<vec4> depth_to_image(cv::Mat &cv_img)
+{
+    auto img = Image<vec4>(cv_img.rows, cv_img.cols);
+    for (int i = 0; i < cv_img.rows; i++) {
+        for (int j = 0; j < cv_img.cols; j++) {
+            auto color = cv_img.at<unsigned short>(i, j);
+            float scaled = color * 1.f/(2 << 16);
+            img(cv_img.rows-1-i,j) = vec4(scaled, scaled, scaled, 1);
+        }
+    }
+    return img;
+}
 
 
 
 
 struct Mesh : public IBehaviour {
-    SurfaceGeometry *geom;
-    Image<vec4> img;
-    GLuint img_tex;
+    int frame_number;
 
-    Mesh(SurfaceGeometry *_geom) : geom{_geom}
-    {
-        cv::Mat cv_img = cv::imread("images/test.png", -1);
-        assert(!img.empty());
-
-        img = Image<vec4>(cv_img.rows, cv_img.cols);
-        for (int i = 0; i < cv_img.rows; i++) {
-            for (int j = 0; j < cv_img.cols; j++) {
-                auto rgb = cv_img.at<cv::Vec3b>(i, j);
-                img(cv_img.rows-1-i,j) = vec4(rgb[0]/256.f, rgb[1]/256.f, rgb[2]/256.f, 1);
+    Mesh() {
+        frame_number = 0;
+    }
+    void keyboard_handler(KeyboardEvent e) {
+        if (e.action == KEYBOARD_PRESS) {
+            if (e.key.code == KEY_M) {
+                frame_number = (frame_number + 1) % frame_metadata.size();
+            }
+            if (e.key.code == KEY_N) {
+                frame_number -= 1;
+                if (frame_number < 0) frame_number = frame_metadata.size()-1;
+            }
+            if (e.key.code == KEY_V) {
+                view_depth = !view_depth;
             }
         }
-        // img(2,2) = vec4(1,0,0,1);
-        // img(4,4) = vec4(0,1,0,1);
-        img_tex = img.texture();
     }
-    void update() {
-        auto &paint = world->graphics.paint;
-        paint.wireframe(*geom, mat4x4::identity(), 0.001);
-        paint.image_3D(img_tex, vec3::zero(), vec3(0,1,0), vec3(sin(total_time),0,cos(total_time)), 3, 3);
-        paint.image_3D(img_tex, vec3(0, 5, 0), vec3(0,1,0), vec3(sin(total_time),0,cos(total_time)), 3, 3);
-        paint.image_3D(img_tex, vec3(5, 0, 0), vec3(0,1,0), vec3(sin(total_time),0,cos(total_time)), 3, 3);
 
+    void update() {
+        
+        auto &paint = world->graphics.paint;
 
         std::vector<vec3> positions;
         for (auto md : frame_metadata) {
-            positions.push_back(md.position);
+            positions.push_back(md.world_position);
+            paint.sphere(md.world_position, 0.02, vec4(1,0,0,1));
         }
         paint.chain(positions, 10, vec4(0,0,0,1));
+
+        int Nx = 60;
+        int Ny = 60;
+        float sphere_size = 0.01;
+        for (int K = 0; K <= frame_number; K++) {
+            auto &md = frame_metadata[K];
+            for (int i = 0; i < Ny; i++) {
+                for (int j = 0; j < Nx; j++) {
+                    float u = i*1.f/(Ny-1);
+                    float v = j*1.f/(Nx-1);
+                    int row = int(u*intrinsics.pixels_y);
+                    int col = int(v*intrinsics.pixels_x);
+                    float z = md.depth_image(row, col).x();
+                    if (z < 1e-5) continue;
+                    vec3 p = frame_to_world(md, vec3(v,u,z));
+                    // float dist = (frame_metadata[frame_number].rotation.transpose() * (p - frame_metadata[frame_number].position)).z();
+                    // float gray = exp(-dist*0.5);
+                    vec4 color = vec4(md.rgb_image(row, col).xyz(), 1);
+                    paint.sphere(p, sphere_size, color);
+                }
+            }
+        }
+
+        {
+            auto md = frame_metadata[frame_number];
+            vec3 up = md.rotation * vec3(0,1,0);
+            vec3 right = md.rotation * vec3(1,0,0);
+
+            float Z = 1;
+            vec3 bl = intrinsic_point(0,0, Z);
+            vec3 tr = intrinsic_point(1,1, Z);
+            float w = tr.x() - bl.x();
+            float h = tr.y() - bl.y();
+
+            vec3 world_bl = frame_to_world(md, vec3(0,0,Z));
+            paint.image_3D(view_depth ? md.depth_tex : md.rgb_tex, world_bl, up, right, w, h);
+    // // DEBUG comparison with fastfusion
+    // for (auto md : frame_metadata) {
+        // std::cout << md.extrinsic_matrix << "\n";
+        // std::cout << md.rotation << "\n";
+        // std::cout << md.world_position << "\n";
+        // getchar();
+        std::cout << md.world_position << "\n";
+        std::cout << intrinsic_point(0,0,1) << "\n";
+        std::cout << md.rotation * intrinsic_point(0,0,1) << "\n";
+    
+        
+      //  getchar();
+    // }
+        vec3 p, q;
+        p = intrinsic_point(0,0,1);
+        q = md.rotation * p;
+        paint.line(md.world_position, md.world_position + q, 5, vec4(0,0,1,1));
+        p = intrinsic_point(1,0,1);
+        q = md.rotation * p;
+        paint.line(md.world_position, md.world_position + q, 5, vec4(0,0,1,1));
+        p = intrinsic_point(1,1,1);
+        q = md.rotation * p;
+        paint.line(md.world_position, md.world_position + q, 5, vec4(0,0,1,1));
+        p = intrinsic_point(0,1,1);
+        q = md.rotation * p;
+        paint.line(md.world_position, md.world_position + q, 5, vec4(0,0,1,1));
+
+            // paint.line(md.world_position, world_bl, 5, vec4(0,0,1,1));
+            // paint.line(md.world_position, world_bl + w*right, 5, vec4(0,0,1,1));
+            // paint.line(md.world_position, world_bl + h*up, 5, vec4(0,0,1,1));
+            // paint.line(md.world_position, world_bl + w*right + h*up, 5, vec4(0,0,1,1));
+        }
+
+
     }
 };
 
@@ -234,73 +342,120 @@ public:
 
 App::App(World &_world) : world{_world}
 {
-    // shader_image = GLShaderProgram();
-    // shader_image->add_shader(GLShader(VertexShader, "shaders/shader_image.vert"));
-    // shader_image->add_shader(GLShader(FragmentShader, "shaders/shader_image.frag"));
-    // shader_image->link();
-
-    // auto v = vec3(1,1,1);
-    //
     Entity cameraman = create_cameraman(world);
     cameraman.get<Transform>()->position = vec3(0,0,2);
     main_camera = cameraman.get<Camera>();
-
-    // Entity obj = create_mesh_object(world, "models/dragon.off", "shaders/uniform_color.mat");
-    // obj.get<Transform>()->position = vec3(0,0,0);
-    // obj.get<Drawable>()->material.properties.set_vec4("albedo", 0.8,0.8,0.8,1);
-
-    auto model = MLModel::load("models/20mm_cube.stl");
-    auto model_geom = new SurfaceGeometry();
-    model_geom->add_model(model);
-    for (auto v : model_geom->vertices()) {
-        model_geom->vertex_positions[v] *= 0.02;
-    }
     Entity e = world.entities.add();
-    auto mesh = world.add<Mesh>(e, model_geom);
+    auto mesh = world.add<Mesh>(e);
 
+    // Set the camera intrinsics (TUM https://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats#intrinsic_camera_calibration_of_the_kinect)
+    intrinsics.fx = 535.4;
+    intrinsics.fy = 539.2;
+    intrinsics.cx = 320.1;
+    intrinsics.cy = 247.6;
+    intrinsics.pixels_x = 640;
+    intrinsics.pixels_y = 480;
+    intrinsics.depth_scale = 5000;
+    intrinsics.intrinsic_matrix = mat3x3(intrinsics.fx,0,0, 0,intrinsics.fy,0, intrinsics.cx,intrinsics.cy,1);
+    auto tmp = mat4x4(vec3(0), intrinsics.intrinsic_matrix).inverse(); //---need mat3x3 inverse...
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            intrinsics.intrinsic_matrix_inverse.entry(i, j) = tmp.entry(i, j);
+        }
+    }
 
     FILE *file = fopen("data/rgbd_tum/associate.txt", "r");
     assert(file != NULL);
     char line[1024];
+    int skip_counter = 0;
+    int get_nth_image = 50;
     while (fgets(line, 1024, file)) {
+        bool skip = skip_counter != 0;
+        skip_counter = (skip_counter + 1) % get_nth_image;
+        if (skip) continue;
         float t;
-        float x1, y1, z1;
-        float w;
-        float x2, y2, z2;
+        float x, y, z;
+        float qw, qx, qy, qz;
         float depth_time;
         char depth_filename[1024];
         float rgb_time;
         char rgb_filename[1024];
         
 // 1341847980.790000 -0.6832 2.6909 1.7373 0.0003 0.8617 -0.5072 -0.0145 1341847980.786879 depth/1341847980.786879.png 1341847980.786856 rgb/1341847980.786856.png
-        size_t num_matched = sscanf(line, "%f %f %f %f %f %f %f %f %f %s %f %s",
-            &t, &x1, &y1, &y2, &w, &x2, &y2, &z2, &depth_time, depth_filename, &rgb_time, &rgb_filename
+        size_t num_matched = sscanf(line, "%f %f %f %f %f %f %f %f %f %s %f %s\n",
+            &t, &x, &y, &z, &qx, &qy, &qz, &qw, &depth_time, depth_filename, &rgb_time, &rgb_filename
         );
         assert(num_matched == 12);
 
         FrameMetadata md;
-        md.position = vec3(x1, y1, z1);
-        md.rotation = vec3(x2, y2, z2);
+        auto rot = Eigen::Quaterniond(qw, qx, qy, qz).toRotationMatrix();
+        mat3x3 R;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                R.entry(i, j) = rot(i, j);
+            }
+        }
+        md.rotation = R;
+        vec3 T = vec3(x, y, z);
+        mat4x4 ext = mat4x4(T, R);
+
         md.t = t;
         md.depth_file = std::string(depth_filename);
         md.rgb_file = std::string(rgb_filename);
-        frame_metadata.push_back(md);
+        md.extrinsic_matrix = ext;
+        md.world_position = T; //---fastfusion
 
         std::cout << "Loaded frame metadata" << "\n";
-        std::cout << "    " << md.position << ", " << md.rotation << "\n";
-        std::cout << "    " << md.depth_file << "\n";
-        std::cout << "    " << md.rgb_file << "\n";
+        std::cout << "    \"" << md.depth_file << "\"\n";
+        std::cout << "    \"" << md.rgb_file << "\"\n";
+        std::cout << "    " << md.t << "\n";
         // getchar();
+
+        frame_metadata.push_back(md);
     }
     fclose(file);
-
-
-    // cv::Mat img = cv::imread("images/test.png", -1);
-    // assert(!img.empty());
-    // cv::namedWindow("ExampleWindow", cv::WINDOW_AUTOSIZE);
-    // cv::imshow("Example1", img);
-    // cv::waitKey(0);
-    // cv::destroyWindow("Example1");
+    std::cout << "Loaded " << frame_metadata.size() << " frames.\n";
+    // getchar();
+    
+    
+    // Change to the coordinates system of the first camera.
+    vec3 c1_pos = frame_metadata[0].world_position;
+    mat3x3 c1_rotation = frame_metadata[0].rotation;
+    for (auto &md : frame_metadata) {
+        md.world_position = c1_rotation.transpose() * (md.world_position - c1_pos);
+        md.rotation = c1_rotation.transpose() * md.rotation;
+        md.extrinsic_matrix = mat4x4(md.world_position, md.rotation);
+    }
+    // fastfusion and the TUM dataset uses an inverted coordinate system, so flip it here.
+    // NOTE: OpenGL rgb & depth images are actually upside down.
+    for (auto &md : frame_metadata) {
+        auto flipmatrix = mat3x3(-1,0,0,  0,-1,0,  0,0,1);
+        md.world_position = flipmatrix * md.world_position;
+        md.rotation = flipmatrix * md.rotation;
+        md.extrinsic_matrix = mat4x4(md.world_position, md.rotation);
+    }
+        
+    // Upload all rgb images to the GPU.
+    for (auto &md : frame_metadata) {
+        // std::cout << name << "\n"; getchar();
+        // cv::Mat cv_img = cv::imread("data/rgbd_tum/rgb/1341847980.722988.png", -1);
+        {
+            std::string name = "data/rgbd_tum/" + std::string(md.rgb_file);
+            cv::Mat cv_img = cv::imread(name, -1);
+            assert(!cv_img.empty());
+            auto img = cv_matrix_to_image_rgba(cv_img);
+            md.rgb_tex = img.texture();
+            md.rgb_image = img;
+        }
+        {
+            std::string name = "data/rgbd_tum/" + std::string(md.depth_file);
+            cv::Mat cv_img = cv::imread(name, -1);
+            assert(!cv_img.empty());
+            auto img = depth_to_distance_image(cv_img);
+            md.depth_tex = depth_to_image(cv_img).texture();
+            md.depth_image = img;
+        }
+    }
 }
 
 void App::close()
